b54a6f8e551680ec803e59db9b96846dd2949764
Shahriar090 <shahriarh655@gmail.com>
2023-07-27 23:36:32 +0600

    added animation on location component

--- src/Layouts/Home/Location/Location.jsx
+++ src/Layouts/Home/Location/Location.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 const Location = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="border-0 m-0 lg:border-4 border-cyan-500 lg:m-8 lg:p-8">
-    <h1 className="text-2xl lg:text-5xl font-bold uppercase text-center text-black m-8 lg:m-10">Find Us On Google Map</h1>
+    <h1 className="text-2xl lg:text-5xl font-bold uppercase text-center text-black m-8 lg:m-10"data-aos="fade-up-right">Find Us On Google Map</h1>
     <span className="divider w-1/2 mx-auto"></span>
     <div className="w-full m-3">
       <div
-        className="hero h-96 w-full lg:w-2/3 lg:mx-auto"
+        className="hero h-96 w-full lg:w-2/3 lg:mx-auto"data-aos="fade-up-left"
         style={{
           backgroundImage:
             "url(https://media.istockphoto.com/id/912922882/vector/vector-city-map-of-hong-kong-with-well-organized-separated-layers.jpg?s=612x612&w=0&k=20&c=aGfHaoxlL5XUqm52hesXu75feyPvB9C1v9AxSkqpKFo=)",
